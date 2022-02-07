c7d1833d5db9c4f47c7cb20fba6b3e9db576c3e1
Anatoli Grishenko <Anatoli-Grishenko@users.noreply.github.com>
2022-02-07 22:44:09 +0100

    Update Rectangle.cpp

--- NetBeansProjects/MPGeometrySep/Rectangle.cpp
+++ NetBeansProjects/MPGeometrySep/Rectangle.cpp
@@ -14,8 +14,8 @@ Rectangle::Rectangle() { }
 Rectangle::Rectangle(int x, int y, int w, int h) {
     topleft.setX(x);
     topleft.setY(y);
-    bottomright.setX(x+w-1);
-    bottomright.setY(y+h-1);
+    bottomright.setX(x+w);
+    bottomright.setY(y-h);
     // setGeometry(x,y,w,h);
 }
 
