d8c4599dd6dcddc8b3819fc915b57fca2664e107
Anatoli Grishenko <Anatoli.Grishenko@gmail.com>
2023-02-14 19:20:01 +0100

    Welcome letter

---
+++ NetBeansProjects/Language0/src/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
+ * @author Andrés Cano Utrera <acu@decsai.ugr.es>
+ * @author Luis Castillo Vidal <L.Castillo@decsai.ugr.es>
+ *
+ * Created on 2 de febrero de 2023, 12:23
+ */
+
+
+/**
+ * This program reads a text (without spaces) with a undefined number of 
+ * characters and a text with two characters (bigram). It finds the bigrams 
+ * contained in the first text, storing them in an array of Bigram. 
+ * After that, the bigrams of the array are shown in the standard output. 
+ * Then it converts to uppercase the bigrams in the array that are equals to the 
+ * bigram of the second text. Finally the bigrams of the array are shown again 
+ * in the standard output. 
+ * Running example:
+ * > dist/Debug/GNU-Linux/language0 < data/SimpleText.txt
+ */
+int main(int argc, char* argv[]) {
+
+}
+
