f1b0a7bb54231d9dc8c3fbd47877792559960bc1
Andrés Cano Utrera <acu@decsai.ugr.es>
2023-03-20 13:35:55 +0100

    Se añade función showEnglishHelp(ostream& ouputStream)

--- NetBeansProjects/Language2/src/main.cpp
+++ NetBeansProjects/Language2/src/main.cpp
@@ -12,12 +12,6 @@
  * Created on 7 de febrero de 2023, 14:02
  */
 
-#include <iostream>
-
-#include "Language.h"
-
-using namespace std;
-
 
 /**
  * Shows help about the use of this program in the given output stream
@@ -34,9 +28,9 @@ void showEnglishHelp(ostream& ouputStream) {
  * passed as parameters to main(). It obtains as result the union of all the 
  * input Language objects. The result is then sorted by decreasing order of 
  * frequency and alphabetical order of bigrams when there is any tie in 
- * frequencies. Finally the result Language is saved in an output file. The 
+ * frequencies. Finally the resulting Language is saved in an output file. The 
  * program must have at least an input file and an output file. 
- * The output Language will have as language identifier, the one of the first 
+ * The output Language will have as language identifier, the one of the first
  * file (<file1.bgr>). If an input file <file*.bgr> has a language identifier
  * different from the one of the first file (<file1.bgr>), then it will not
  * be included in the union.
@@ -44,22 +38,6 @@ void showEnglishHelp(ostream& ouputStream) {
  * > language2 <file1.bgr> [<file2.bgr> ... <filen.bgr>] <outputFile.bgr> 
  */
 int main(int argc, char* argv[]) {
-    Language inputLanguage, inputLanguageAux;
 
-    if (argc < 3) {
-        showEnglishHelp(cerr);
-        return 1;
-    }
-
-    inputLanguage.load(argv[1]);
-    for (int i = 2; i < argc - 1; i++) {
-        inputLanguageAux.load(argv[i]);
-        if (inputLanguageAux.getLanguageId() == inputLanguage.getLanguageId()) {
-            inputLanguage.join(inputLanguageAux);
-        }
-    }
-    inputLanguage.sort();
-    inputLanguage.save(argv[argc - 1]);
-    return 0;
 }
 
