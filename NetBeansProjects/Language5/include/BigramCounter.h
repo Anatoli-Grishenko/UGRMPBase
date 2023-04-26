5a39a93b1014f9f7377b0af5d526c20699e7e04e
Andrés Cano Utrera <acu@decsai.ugr.es>
2023-04-26 14:01:48 +0200

    frecuency changed by frequency

--- NetBeansProjects/Language5/include/BigramCounter.h
+++ NetBeansProjects/Language5/include/BigramCounter.h
@@ -19,9 +19,9 @@
 
 /**
  * @class BigramCounter
- * @brief It is a helper class used to calculate the frecuency of each bigram in
+ * @brief It is a helper class used to calculate the frequency of each bigram in
  * a text file. It consists of a square matrix of integers. Each element contains
- * the frecuency of the bigram that defines that element: the bigram formed taking
+ * the frequency of the bigram that defines that element: the bigram formed taking
  * the characters defined by the row and column of that element. 
  */
 class BigramCounter {
@@ -73,28 +73,28 @@ public:
     int getNumberActiveBigrams();
 
     /**
-     * @brief Sets the frecuency of the given bigram using the value 
-     * provided with @p frecuency. Modifier method
+     * @brief Sets the frequency of the given bigram using the value 
+     * provided with @p frequency. Modifier method
      * @param bigram The bigram in which the frequency will be set. Input
      * parameter
-     * @param frecuency The new frecuency. Input parameter
+     * @param frequency The new frequency. Input parameter
      * @return true if the bigram was found in this object. false otherwise
      */
-    bool setFrecuency(Bigram bigram, int frecuency);
+    bool setFrequency(Bigram bigram, int frequency);
 
     /**
-     * @brief Increases the current frecuency of the given bigram using the value 
-     * provided with @p frecuency. If @p frecuency is 0 or frecuency is not 
-     * provided, then 1 is added to the current frecuency of the bigram.
+     * @brief Increases the current frequency of the given bigram using the value 
+     * provided with @p frequency. If @p frequency is 0 or frequency is not 
+     * provided, then 1 is added to the current frequency of the bigram.
      * Modifier method
      * @throw std::invalid_argument This method throws an 
      * std::invalid_argument exception if the given bigram is not valid
      * @param bigram The bigram in which the frequency will be modified.
      * Input parameter
-     * @param frecuency The quantity that will be added to the current frecuency.
+     * @param frequency The quantity that will be added to the current frequency.
      * Input parameter
      */
-    void increaseFrecuency(Bigram bigram, int frecuency = 0);
+    void increaseFrequency(Bigram bigram, int frequency = 0);
 
     /**
      * @brief Overloading of the assignment operator
@@ -129,14 +129,14 @@ public:
     /**
      * @brief Builds a Language object from this BigramCounter object. The 
      * Language will contain the bigrams and frecuencies for those one with 
-     * a frecuency greater than 0.
+     * a frequency greater than 0.
      * Query method
      * @return A Language object from this BigramCounter object
      */
     Language toLanguage();
 
 private:
-    int** _frequency; ///< 2D matrix with the frecuency of each bigram
+    int** _frequency; ///< 2D matrix with the frequency of each bigram
 
     /**
      * Set of characters that are considered as part of a word. Any other 
