1d150cf247a7c027a7a4494b1c81a87bb804ea19
Andrés Cano Utrera <acu@decsai.ugr.es>
2023-05-05 15:21:13 +0200

    Updating because of changes in names of executables

---
+++ NetBeansProjects/MyLanguage4/src/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * Metodología de la Programación: Language4
+ * Curso 2022/2023
+ */
+
+/* 
+ * File:   main.cpp
+ * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
+ * @author Andrés Cano Utrera <acu@decsai.ugr.es>
+ * @author Luis Castillo Vidal <L.Castillo@decsai.ugr.es>
+ *
+ * Created on 12 February 2023, 10:40
+ */
+
+/**
+ * Shows help about the use of this program in the given output stream
+ * @param outputStream The output stream where the help will be shown (for example,
+ * cout, cerr, etc) 
+ */
+void showEnglishHelp(ostream& outputStream) {
+    outputStream << "Error, run with the following parameters:" << endl;
+    outputStream << "language4 [-t min|max] <file1.bgr> <file2.bgr> [ ... <filen.bgr>]" << endl;
+}
+
+/**
+ * This program reads an undefined number of Language objects from the set of 
+ * files passed as parameters to main(). All the Languages object, except the 
+ * first one, must be stored in a dynamic array of Language objects. Then, 
+ * for each Language in the dynamic array, this program prints to the 
+ * standard output the name of the file of that Language and the distance from 
+ * the first Language to the current Language. 
+ * Finally, the program should print in the standard output, the name of 
+ * the file with the Language with the minimum|maximum distance to the Language 
+ * of the first file and its language identifier.
+ * 
+ * At least, two Language files are required to run this program.
+ * Running example:
+ * > language4 [-t min|max] <file1.bgr> <file2.bgr> [  ... <filen.bgr>] 
+ */
+int main(int argc, char* argv[]) {
+}
+
